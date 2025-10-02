2f0fc9e57c000ff2a4886f493b531a5d4ca4d37c
Hatem Ayed <ixovhh@gmail.com>
2025-10-02 15:26:00 +0300

    update theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,7 +4,7 @@ import 'package:hackathon/theme/app_color.dart';
 
 
 class AppTheme {
-  static ThemeData themelightMode() {
+  static ThemeData themeApp() {
     return ThemeData(
       textTheme: TextTheme(
         // for heading and page title
@@ -31,45 +31,11 @@ class AppTheme {
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
         type: BottomNavigationBarType.fixed,
         backgroundColor: AppColor.lightBackGround,
-        selectedItemColor: Colors.deepPurpleAccent.shade700,
+        selectedItemColor: Color(0xFF00AA5B),
         unselectedItemColor: Colors.grey.shade600,
       ),
 
     );
   }
 
-  static ThemeData themedarkMode() {
-    return ThemeData(
-      textTheme: TextTheme(
-        // for heading and page title
-        titleLarge: GoogleFonts.roboto(
-          color: AppColor.darkMainTextColor,
-          fontSize: 30,
-          fontWeight: FontWeight.bold,
-        ),
-        // for main text
-        titleMedium: GoogleFonts.roboto(
-          color: AppColor.darkMainTextColor,
-          fontSize: 20,
-          fontWeight: FontWeight.w600,
-        ),
-
-        titleSmall: GoogleFonts.roboto(
-          color: AppColor.darkMainTextColor,
-          fontSize: 12,
-          fontWeight: FontWeight.w600,
-        ),
-      ),
-
-      scaffoldBackgroundColor: AppColor.darkBackGround,
-      appBarTheme: AppBarTheme(backgroundColor: AppColor.darkBackGround),
-
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: AppColor.darkBackGround,
-        selectedItemColor: AppColor.darkMainTextColor,
-        unselectedItemColor: Colors.grey.shade600,
-      ),
-    );
-  }
 }
